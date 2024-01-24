c2b16ebb7837761a292e108d513eea2ddb993f7d
ikhwanhsn <ikhwanulhusna111@gmail.com>
2024-01-24 11:39:26 +0700

    Chore: add spring boot

---
+++ src/test/java/decomus/coffeShop/CoffeShopApplicationTests.java
@@ -0,0 +1,13 @@
+package decomus.coffeShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
